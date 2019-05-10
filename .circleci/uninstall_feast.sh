a6d3e9a66fa6708e5f6f79cf839a4e2b7fcb90cf
David Heryanto <david.heryanto@hotmail.com>
2019-05-11 01:40:38 +0800

    Fix build num

--- .circleci/uninstall_feast.sh
+++ .circleci/uninstall_feast.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export FEAST_RELEASE_NAME=feast-${CIRCLE_BUILD_NUM}
+export FEAST_RELEASE_NAME=feast-${CIRCLE_SHA1}
 
 echo ${GCLOUD_SERVICE_KEY} | gcloud auth activate-service-account --key-file=-
 gcloud container clusters get-credentials feast-test-cluster --zone us-central1-a --project kf-feast
