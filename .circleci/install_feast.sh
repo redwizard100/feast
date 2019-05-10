f21ee7307e215b5e29eead0f3fe6c358eb023b46
David Heryanto <david.heryanto@hotmail.com>
2019-05-11 01:32:11 +0800

    Use export

--- .circleci/install_feast.sh
+++ .circleci/install_feast.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-FEAST_BUILD_NUMBER=${CIRCLE_BUILD_NUM}
-FEAST_IMAGE_TAG=1890cb04688a3e10f6de7305992e71ee7ba1793d
-# FEAST_IMAGE_TAG=${CIRCLE_SHA1}
-FEAST_WAREHOUSE_DATASET=feast_build_${CIRCLE_BUILD_NUM}
-FEAST_CORE_URL=build-${CIRCLE_BUILD_NUM}.feast.dragonin.me:80
-FEAST_SERVING_URL=build-${CIRCLE_BUILD_NUM}.feast.dragonin.me:80
-FEAST_RELEASE_NAME=feast-${CIRCLE_BUILD_NUM}
+export FEAST_BUILD_NUMBER=${CIRCLE_BUILD_NUM}
+export FEAST_IMAGE_TAG=1890cb04688a3e10f6de7305992e71ee7ba1793d
+# export FEAST_IMAGE_TAG=${CIRCLE_SHA1}
+export FEAST_WAREHOUSE_DATASET=feast_build_${CIRCLE_BUILD_NUM}
+export FEAST_CORE_URL=build-${CIRCLE_BUILD_NUM}.feast.dragonin.me:80
+export FEAST_SERVING_URL=build-${CIRCLE_BUILD_NUM}.feast.dragonin.me:80
+export FEAST_RELEASE_NAME=feast-${CIRCLE_BUILD_NUM}
 
 echo ${GCLOUD_SERVICE_KEY} | gcloud auth activate-service-account --key-file=-
 gcloud container clusters get-credentials feast-test-cluster --zone us-central1-a --project kf-feast
--- .circleci/uninstall_feast.sh
+++ .circleci/uninstall_feast.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FEAST_RELEASE_NAME=feast-${CIRCLE_BUILD_NUM}
+export FEAST_RELEASE_NAME=feast-${CIRCLE_BUILD_NUM}
 
 echo ${GCLOUD_SERVICE_KEY} | gcloud auth activate-service-account --key-file=-
 gcloud container clusters get-credentials feast-test-cluster --zone us-central1-a --project kf-feast
