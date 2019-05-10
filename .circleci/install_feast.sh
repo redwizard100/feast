ae2857c8ff34597dbb18bb636d686f8fd3c76c83
David Heryanto <david.heryanto@hotmail.com>
2019-05-11 01:25:16 +0800

    Test directories

--- .circleci/install_feast.sh
+++ .circleci/install_feast.sh
@@ -11,6 +11,13 @@ FEAST_RELEASE_NAME=feast-${CIRCLE_BUILD_NUM}
 echo ${GCLOUD_SERVICE_KEY} | gcloud auth activate-service-account --key-file=-
 gcloud container clusters get-credentials feast-test-cluster --zone us-central1-a --project kf-feast
 
+pwd 
+ls -lh
+echo ${CIRCLE_WORKING_DIRECTORY}
+ls -lh ${CIRCLE_WORKING_DIRECTORY}
+echo ${HOME}
+ls -lh ${HOME}
+
 cd ${CIRCLE_WORKING_DIRECTORY}/integration-tests
 envsubst < feast-helm-values.yaml.template > feast-helm-values.yaml
 helm install --name ${FEAST_RELEASE_NAME} --wait --timeout 210 ../charts/feast -f feast-helm-values.yaml
