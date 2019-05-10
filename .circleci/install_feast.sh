1117ce5af6e75fe3cb3c75240474d312a07856d7
David Heryanto <david.heryanto@hotmail.com>
2019-05-11 01:06:09 +0800

    Fix workdir path

--- .circleci/install_feast.sh
+++ .circleci/install_feast.sh
@@ -10,6 +10,6 @@ FEAST_RELEASE_NAME=feast-${CIRCLE_BUILD_NUM}
 echo ${GCLOUD_SERVICE_KEY} | gcloud auth activate-service-account --key-file=-
 gcloud container clusters get-credentials feast-test-cluster --zone us-central1-a --project kf-feast
 
-cd $FEAST_HOME/integration-tests
+cd ${CIRCLE_WORKING_DIRECTORY}/integration-tests
 envsubst < feast-helm-values.yaml.template > feast-helm-values.yaml
-helm install --name $FEAST_RELEASE_NAME --wait --timeout 210 ../charts/feast -f feast-helm-values.yaml
+helm install --name ${FEAST_RELEASE_NAME} --wait --timeout 210 ../charts/feast -f feast-helm-values.yaml
