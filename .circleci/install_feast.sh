57d80f6c68a28031a4f429f847bbf8a60088ec61
David Heryanto <david.heryanto@hotmail.com>
2019-05-11 20:51:54 +0800

    Fix path to chart

--- .circleci/install_feast.sh
+++ .circleci/install_feast.sh
@@ -7,7 +7,7 @@ export FEAST_WAREHOUSE_DATASET=feast_build_${CIRCLE_SHA1:0:7}
 export FEAST_RELEASE_NAME=feast-${CIRCLE_SHA1:0:7}
 
 envsubst < integration-tests/feast-helm-values.yaml.template > integration-tests/feast-helm-values.yaml
-helm install --name ${FEAST_RELEASE_NAME} --wait --timeout 210 ../charts/feast -f feast-helm-values.yaml
+helm install --name ${FEAST_RELEASE_NAME} --wait --timeout 210 ./charts/feast -f feast-helm-values.yaml
 
 kubectl port-forward service/${FEAST_RELEASE_NAME}-core 50051:6565 &
 kubectl port-forward service/${FEAST_RELEASE_NAME}-serving 50052:6565 &
