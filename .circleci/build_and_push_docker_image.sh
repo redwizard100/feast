788011fe21bd2708d0ef968a68d1d760566ef07d
David Heryanto <david.heryanto@hotmail.com>
2019-05-11 22:15:10 +0800

    Fix build_and_push_docker_image script

--- .circleci/build_and_push_docker_image.sh
+++ .circleci/build_and_push_docker_image.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: build_and_push_docker_image.sh REGISTRY/REPO:TAG"
-    echo "Example: build_and_push_docker_image.sh gcr.io/project/app:1.0.0"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: build_and_push_docker_image.sh COMPONENT REGISTRY/REPO:TAG"
+    echo "Example: build_and_push_docker_image.sh core gcr.io/project/feast-core:1.0.0"
+    echo "         build_and_push_docker_image.sh serving gcr.io/project/feast-serving:1.0.0"
     exit 1
 fi
 
-DOCKER_IMAGE_TAG=${1}
+COMPONENT=${1}
+DOCKER_IMAGE_TAG=${2}
 
 echo ${GCLOUD_SERVICE_KEY} | gcloud auth activate-service-account --key-file=-
 gcloud -q auth configure-docker
 
-docker build -t ${DOCKER_IMAGE_TAG} -f Dockerfiles/core/Dockerfile .
+docker build -t ${DOCKER_IMAGE_TAG} -f Dockerfiles/${COMPONENT}/Dockerfile .
 docker push ${DOCKER_IMAGE_TAG}
