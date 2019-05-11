7caae2ac2b2124f7344954291985a14037e20e02
David Heryanto <david.heryanto@hotmail.com>
2019-05-11 21:35:30 +0800

    Fix gsutil installation in build push cli

--- .circleci/build_and_push_cli.sh
+++ .circleci/build_and_push_cli.sh
@@ -9,8 +9,10 @@ fi
 GCS_URI=${1}
 
 # Install Google Cloud SDK
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-. ${SCRIPT_DIR}/install_google_cloud_sdk.sh
+GOOGLE_CLOUD_SDK_ARCHIVE_URL=https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-244.0.0-linux-x86_64.tar.gz
+wget -qO- ${GOOGLE_CLOUD_SDK_ARCHIVE_URL} | tar xz -C /
+export PATH=/google-cloud-sdk/bin:$PATH
+echo ${GCLOUD_SERVICE_KEY} | gcloud auth activate-service-account --key-file=-
 
 # Build CLI and upload to GCS
 go build -o ./cli/build/feast ./cli/feast
