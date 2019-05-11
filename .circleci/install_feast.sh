23b89afe3efe727dd0fcda33c3a6acbd5b523e1c
David Heryanto <david.heryanto@hotmail.com>
2019-05-11 21:06:59 +0800

    Fix missing env var

--- .circleci/install_feast.sh
+++ .circleci/install_feast.sh
@@ -4,6 +4,7 @@ export FEAST_BUILD_NUMBER=${CIRCLE_SHA1:0:7}
 export FEAST_IMAGE_TAG=1890cb04688a3e10f6de7305992e71ee7ba1793d
 # export FEAST_IMAGE_TAG=${CIRCLE_SHA1}
 export FEAST_WAREHOUSE_DATASET=feast_build_${CIRCLE_SHA1:0:7}
+export FEAST_RELEASE_NAME=feast-${CIRCLE_SHA1:0:7}
 
 envsubst < integration-tests/feast-helm-values.yaml.template > integration-tests/feast-helm-values.yaml
 helm install --name ${FEAST_RELEASE_NAME} --wait --timeout 210 ./charts/feast -f integration-tests/feast-helm-values.yaml
